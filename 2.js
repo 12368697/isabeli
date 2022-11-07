7eccb242fea89787b83a7f957fa6b62c4b3886cf
12368697 <115477457+12368697@users.noreply.github.com>
2022-11-07 08:39:09 -0300

    javascript

---
+++ 2.js
@@ -0,0 +1,4 @@
+var p1 = prompt("digite uma palavra aleatória")
+var p2 = prompt("digite uma palavra aleatória")
+var p3 = prompt("digite uma palavra aleatória")
+console.log(p1 + " foi ao casamento da(o) " + p2 + " no dia da(o)" + p3)
